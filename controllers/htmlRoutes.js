445d2f490fd85e88c9212e6e4c8fc511f7845f77
Maverick <embrymaverick@gmail.com>
2018-05-15 22:21:40 -0700

    added a test route for profile page

--- controllers/htmlRoutes.js
+++ controllers/htmlRoutes.js
@@ -5,4 +5,9 @@ module.exports = function(app) {
     app.get('/', function(req, res) {
         res.sendFile(path.join(__dirname + "/../views/main.html"));
     })
+    app.get('/:name', function(req, res) {
+        var name = req.body.name;
+        console.log(name);
+        res.render(path.join(__dirname + "/../views/profile.handlebars"));
+    })
 }
