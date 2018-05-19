f40ddcae84fe23b64db9dc054638e190019adfcf
Maverick <embrymaverick@gmail.com>
2018-05-19 12:02:38 -0700

    profile js

---
+++ public/js/profile.js
@@ -0,0 +1,43 @@
+$(document).ready(function(){
+var db = require('../../models');
+
+function changeImage(a) {
+  event.preventDefault();
+  document.getElementById("profile-image1").src = a;
+}
+
+$(".profile").on("click", function () {
+  event.preventDefault();
+  $(".profbutton").attr('style', 'visibility: visible');
+  $(".saveProfile").attr('style', 'visibility: visible');
+
+})
+
+$(".saveProfile").on("click", function () {
+      event.preventDefault();
+      $(".profbutton").attr('style', 'visibility: hidden');
+      $(".saveProfile").attr('style', 'visibility: hidden');
+
+      var profileImg = document.getElementById("profile-image1").src;
+
+      app.get('/profile/:id', function (req, res) {
+        var id = req.params.id;
+
+        db.playerProfile.findOne({
+            where: {
+              id: id
+            },
+          })
+          .on('success', function (profile) {
+            // Check if record exists in db
+            if (profile) {
+              project.updateAttributes({
+                  profilePic: profileImg
+                })
+                .success(function () {})
+            }
+          })
+        });
+    })
+});
+    
