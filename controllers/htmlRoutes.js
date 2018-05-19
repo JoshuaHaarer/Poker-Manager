863bf624b8191ba68a8b35824fc0645261a0487f
Kyle <kyle.folkerth@gmail.com>
2018-05-19 11:54:15 -0700

    updates html and api routes

--- controllers/htmlRoutes.js
+++ controllers/htmlRoutes.js
@@ -6,9 +6,12 @@ module.exports = function(app) {
         //Home page should be where the user selects either player or manager, then sends to login page 
         res.sendFile(path.join(__dirname + "/../views/main.html"));
     })
-    app.get('/:name', function(req, res) {
-        var name = req.body.name;
+    app.get('/profile/:id', function(req, res) {
+        var name = req.params.id;
         console.log(name);
         res.render(path.join(__dirname + "/../views/profile.handlebars"));
+    })
+    app.get('/creategame', function(req, res) {
+        res.sendFile(path.join(__dirname + "/../views/creategame.html"));
     })
 }
