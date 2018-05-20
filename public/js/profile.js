6f730f62381b172b9213287049d491457acfeddb
Maverick <embrymaverick@gmail.com>
2018-05-20 16:52:07 -0700

    moved images

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,43 +1,45 @@
 $(document).ready(function(){
-var db = require('../../models');
+
 
 function changeImage(a) {
   event.preventDefault();
   document.getElementById("profile-image1").src = a;
-}
+  console.log(a);
+};
 
 $(".profile").on("click", function () {
   event.preventDefault();
   $(".profbutton").attr('style', 'visibility: visible');
   $(".saveProfile").attr('style', 'visibility: visible');
 
-})
+});
 
 $(".saveProfile").on("click", function () {
       event.preventDefault();
       $(".profbutton").attr('style', 'visibility: hidden');
       $(".saveProfile").attr('style', 'visibility: hidden');
 
-      var profileImg = document.getElementById("profile-image1").src;
+    //   var profileImg = document.getElementById("profile-image1").src;
 
-      app.get('/profile/:id', function (req, res) {
-        var id = req.params.id;
+    //   app.get('/profile/:id', function (req, res) {
+    //     var id = req.params.id;
 
-        db.playerProfile.findOne({
-            where: {
-              id: id
-            },
-          })
-          .on('success', function (profile) {
-            // Check if record exists in db
-            if (profile) {
-              project.updateAttributes({
-                  profilePic: profileImg
-                })
-                .success(function () {})
-            }
-          })
-        });
-    })
+    //     db.playerProfile.findOne({
+    //         where: {
+    //           id: id
+    //         },
+    //       })
+    //       .on('success', function (profile) {
+    //         // Check if record exists in db
+    //         if (profile) {
+    //           project.updateAttributes({
+    //               profilePic: profileImg
+    //             })
+    //             .success(function () {})
+    //         }
+    //       })
+    //     });
+    // })
+});
 });
     
