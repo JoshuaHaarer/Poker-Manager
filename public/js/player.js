937da066ad4525d64d6a06e6655d49ccdc3bf976
Maverick <embrymaverick@gmail.com>
2018-05-20 22:02:18 -0700

    tweaking player.js

--- public/js/player.js
+++ public/js/player.js
@@ -15,34 +15,42 @@ $(document).ready(function () {
     $(proForm).on("click", function () {
         event.preventDefault();
 
+        //User input values
         var userName = $("#userName").val();
         var nickName = $("#nickName").val();
         var firstName = $("#firstName").val();
         var lastName = $("#lastName").val();
         var email = $("#email").val();
+
+        //Submit button
         var proForm = $("#proForm");
 
         
-
+        //If they enter nothing it will return false
         if (userName === "") {
+            alert("Please fill every field!");
             return;
         } else if (nickName === "") {
+            alert("Please fill every field!");
             return;
         } else if (firstName === "") {
+            alert("Please fill every field!");
             return;
         } else if (lastName === "") {
+            alert("Please fill every field!");
             return;
         } else if (email === "") {
+            alert("Please fill every field!");
             return;
         };
 
 
         var newProfile = {
-            userName: userName,
-            nickName: nickName,
-            firstName: firstName,
-            lastName: lastName,
-            email: email
+            userName: userName.trim(),
+            nickName: nickName.trim(),
+            firstName: firstName.trim(),
+            lastName: lastName.trim(),
+            email: email.trim()
         };
 
         console.log(newProfile);
